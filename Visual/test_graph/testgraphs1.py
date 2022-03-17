2923db022da9b4967078e6237a99a6e992fb876d
Brian Brungardt <behrtarp@outlook.com>
2022-03-17 12:52:00 -0500

    2 graphs

--- Visual/test_graph/testgraphs1.py
+++ Visual/test_graph/testgraphs1.py
@@ -1,4 +1,5 @@
 import os
+from tkinter import font as ft
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -9,10 +10,15 @@ g_results = pd.read_csv(os.path.join('data', 'clean', 'game_results_clean.csv'))
 
 print(g_results)
 
-plt.scatter(g_results.team1_srs, g_results.team2_srs)
+srscorr = plt.scatter(g_results.team1_srs, g_results.team2_srs, color = 'brown')
 plt.xlabel('Team1 SRS')
 plt.ylabel('Team2 SRS')
 plt.title('SRS Correlation')
 plt.show()
 
 
+rpicorr = plt.scatter(g_results.team1_rpi, g_results.team1_rpi, color = 'blue')
+plt.xlabel('Team1 RPI')
+plt.ylabel('Team2 RPI')
+plt.title('RPI Correlation')
+plt.show()
