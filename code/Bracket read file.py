ad4eb8c6239dd8816d6c9f059b2ae830f7001c05
Andy_Wang <andy.ct.wang@utexas.edu>
2022-03-21 17:35:24 -0500

    changed bracket_data.csv path to relative

--- code/Bracket read file.py
+++ code/Bracket read file.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
 
-df1 = pd.read_csv(os.path.join("C:/Users/madwo/ncaa_rankings/data/clean", "2022_teams.csv"))
-df2 = pd.read_csv(os.path.join("C:/Users/madwo/ncaa_rankings/data/clean", "team-ratings.csv"))
+df1 = pd.read_csv(os.path.join("data/clean", "2022_teams.csv"))
+df2 = pd.read_csv(os.path.join("data/clean", "team-ratings.csv"))
 df2 = df2[df2.year == 2022]
 w_names = ["Saint Mary's (CA)", "Louisiana State", "Miami (FL)", "Loyola (IL)", "UAB"]
 c_names = ["St. Mary's (Cal.)", "LSU", "Miami (Fla.)", "Loyola (Ill.)", "Alabama-Birmingham"]
@@ -11,4 +11,4 @@ for w in w_names:
 bracket_data = pd.merge(df1, df2[["team", "osrs", "dsrs"]], on = "team",
                         how = "left")
 bracket_data.rename(columns = {'osrs_y': 'osrs', 'dsrs_y': 'dsrs'}, inplace = True)
-bracket_data.to_csv(os.path.join("C:/Users/madwo/ncaa_rankings/data/clean", "bracket_data.csv"), index = False)
+bracket_data.to_csv(os.path.join("data/clean", "bracket_data.csv"), index = False)
