2f34a73d552c9ae1cbf9aef78779477517fcd9a2
crawleyliam <liam.crawley821@gmail.com>
2022-03-22 12:06:57 -0500

    correct quotes in testgraphs1 file

--- code/visual code/testgraphs1.py
+++ code/visual code/testgraphs1.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 
 """cd ~/ncaa_rankings"""
 
-g_results = pd.read_csv(os.path.join('data', 'clean', 'game_results_clean.csv'))
+g_results = pd.read_csv(os.path.join("data", "clean", "game_results_clean.csv"))
 
 print(g_results)
 
 plt.scatter(g_results.team1_srs, g_results.team2_srs)
-plt.xlabel('Team1 SRS')
-plt.ylabel('Team2 SRS')
-plt.title('SRS Correlation')
+plt.xlabel("Team1 SRS")
+plt.ylabel("Team2 SRS")
+plt.title("SRS Correlation")
 plt.show()
 
 
