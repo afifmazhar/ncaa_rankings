ca35e955bba8f4348600258a1a33a879fd158fa1
Brian Brungardt <behrtarp@outlook.com>
2022-03-22 13:16:52 -0500

    Final Edit of Chart code

--- code/visual_code/final-data-table.py
+++ code/visual_code/final-data-table.py
@@ -4,10 +4,10 @@ import plotly.graph_objects as pt
 import os
 import pandas as pd
 
-g_results = pd.read_csv(os.path.join("data", "clean", "matchups.csv"), names = ["Game_Num", "Matched_Teams1", "Matched_Teams2"])
+g_results = pd.read_csv(os.path.join("data", "clean", "winners.csv"), names = ["Game_Num", "Winning_Team"])
 
-winner_table = pt.Figure(data = [pt.Table(header = dict(values = list(g_results.columns), fill_color = "cyan", align = "center", font = dict(size = 16)),
-cells = dict(values = [g_results["Game_Num"], g_results["Matched_Teams1"], g_results["Matched_Teams2"]], fill_color = "lavender", font = dict(size = 11)))]) 
+winner_table = pt.Figure(data = [pt.Table(header = dict(values = list(g_results.columns), fill_color = "coral", align = "center", font = dict(size = 16)),
+cells = dict(values = [g_results["Game_Num"], g_results["Winning_Team"]], fill_color = "lavender", font = dict(size = 11)))]) 
 
 winner_table.show()
 
