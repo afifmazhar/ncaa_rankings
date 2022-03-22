05d5171ea1da323c08858c871d0ce1376c4eeeb3
taylorneal <taylor.neal@utexas.edu>
2022-03-22 14:39:48 +0000

    standardize comments across scripts.

--- code/combine-games-with-srs.py
+++ code/combine-games-with-srs.py
@@ -2,18 +2,18 @@
 import os
 import pandas as pd
 
-"read in csv"
+## read in csv
 team_ratings = pd.read_csv(os.path.join("data", "clean", "team-ratings.csv"))
 game_results = pd.read_csv(os.path.join("data", "clean", "game_results_clean.csv"))
 
-"rename colleges so that they are consistent accross data sets"
+## rename colleges so that they are consistent accross data sets
 bad_names = ["Louisiana State", "Murray State", "Loyola (IL)", "Miami (FL)", "Miami (OH)", "Pennsylvania", "UAB", "Saint Louis", "Saint Joseph's", "Loyola (MD)", "Louisiana", "St. John's (NY)", "Saint Mary's (CA)", "Long Island University", "Middle Tennessee", "Little Rock", "NC State", "Albany (NY)", "Cal Poly", "Nevada-Las Vegas", "UTSA", "Detroit Mercy", "Cal State Long Beach", "Saint Peter's", "UT Arlington"]
 good_names = ["LSU", "Murray St.", "Loyola (Ill.)", "Miami (Fla.)", "Miami (Ohio)", "Penn", "Alabama-Birmingham", "St. Louis", "St. Joseph's", "Loyola (Md.)", "Louisiana-Lafayette", "St. John's", "St. Mary's (Cal.)", "Long Island", "Middle Tennessee State", "Arkansas-Little Rock", "North Carolina State", "Albany (N.Y.)", "Cal-Poly", "UNLV", "Texas-San Antonio", "Detroit", "Long Beach State", "St. Peter's", "Texas-Arlington"]
 
 for bad in bad_names:
     team_ratings.loc[team_ratings["team"] == bad, "team"] = good_names[bad_names.index(bad)]
 
-"match team ratings to their teams"
+## match team ratings to their teams
 team_ratings = team_ratings.set_index(["team", "year"])
 game_results = game_results.set_index(["team1", "year"])
 
@@ -34,7 +34,7 @@ game_results = game_results.assign(
     team2_srs = team_ratings["srs"]
 )
 
-"generate the updated csv"
+## generate the updated csv
 game_results = game_results.reset_index()
 
 game_results.to_csv(os.path.join("data", "clean", "game_results_clean.csv"), index = False)
