dc404b8ecb80b768187b58d7f8003da6bb69ee5c
crawleyliam <liam.crawley821@gmail.com>
2022-03-23 10:52:47 -0500

    Visual to visual and more quote fixes

--- code/testgraphs1.py
+++ code/testgraphs1.py
@@ -4,18 +4,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-g_results = pd.read_csv(os.path.join('data', 'clean', 'game_results_clean.csv'))
+g_results = pd.read_csv(os.path.join("data", "clean", "game_results_clean.csv"))
 
 ## Create correlation plot of SRS ratings
-srscorr = plt.scatter(g_results.team1_srs, g_results.team2_srs, color = 'brown')
-plt.xlabel('Team1 SRS')
-plt.ylabel('Team2 SRS')
-plt.title('SRS Correlation')
+srscorr = plt.scatter(g_results.team1_srs, g_results.team2_srs, color = "brown")
+plt.xlabel("Team1 SRS")
+plt.ylabel("Team2 SRS")
+plt.title("SRS Correlation")
 plt.show()
 
 ## Create correlation plot of RPI ratings
-rpicorr = plt.scatter(g_results.team1_rpi, g_results.team1_rpi, color = 'blue')
-plt.xlabel('Team1 RPI')
-plt.ylabel('Team2 RPI')
-plt.title('RPI Correlation')
+rpicorr = plt.scatter(g_results.team1_rpi, g_results.team1_rpi, color = "blue")
+plt.xlabel("Team1 RPI")
+plt.ylabel("Team2 RPI")
+plt.title("RPI Correlation")
 plt.show()
